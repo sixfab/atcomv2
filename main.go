d0b21090f89f42d47727d788b45ac7defee1e681
Yasin Kaya <yasinkaya.121@gmail.com>
2023-10-20 21:30:53 +0300

    Create base structure of cli tool

---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+Created by: Yasin Kaya (selengalp), yasinkaya.121@gmail.com, 2023
+
+Copyright (c) 2023 Sixfab Inc.
+*/
+package main
+
+import "github.com/sixfab/atcomv2/pkg/cmd"
+
+func main() {
+	cmd.Execute()
+}
