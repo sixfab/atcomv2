02a276d153b908e4c96892b8133254c306106a9d
Yasin Kaya <yasinkaya.121@gmail.com>
2023-10-31 22:23:04 +0300

    fix: Change the serial library with the one has timeout feature

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+const Version = "0.1.0"
+
+// cli configs
+var Verbose bool = true
--- main.go
+++ main.go
@@ -5,8 +5,26 @@ Copyright (c) 2023 Sixfab Inc.
 */
 package main
 
-import "github.com/sixfab/atcomv2/pkg/cmd"
+import (
+	"fmt"
+)
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
+
+	args := make(map[string]interface{})
+	args["port"] = "/dev/ttyUSB3"
+	args["baud"] = 115200
+	args["desired"] = []string{"+CREG: 0,1", "+CREG: 0,5"}
+	args["fault"] = []string{"+CREG: 0,2", "+CREG: 0,3", "+CREG: 0,4"}
+	args["lineEnd"] = true
+	args["timeout"] = 5
+
+	res, err := sendAT("AT+CREG?", args)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
 }
